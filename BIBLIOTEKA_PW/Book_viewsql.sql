80781110fcddb28575c3ab2b779fd6f3bd3028b5
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2024-03-17 11:58:15 +0100

    Book_view

---
+++ BIBLIOTEKA_PW/Book_viewsql.sql
@@ -0,0 +1,17 @@
+select
+Books.Book_ID,
+Books.Tytul,
+Books.Opis,
+Books.Rok_wydania,
+Books.Ilosc_szt,
+Wydawnictwo.Nazwa as Wydawnictwo,
+Kategoria.Nazwa as Kategoria,
+Recenzje.Ocena,
+Recenzje.Opinia
+from Books
+Left join 
+Wydawnictwo on Books.Wydawnictwo_ID=Wydawnictwo.Wydawnictwo_ID
+Left join
+Kategoria on Books.Kategoria_ID=Kategoria.Kategoria_ID
+Left join
+Recenzje on Books.Recenzja_ID=Recenzje.Recenzja_ID
