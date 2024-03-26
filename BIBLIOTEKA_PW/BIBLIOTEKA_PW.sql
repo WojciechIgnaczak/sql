f94316ae9b530505a0a8216adf571ff3078f7e27
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2024-03-26 19:07:15 +0100

    librarypw+insert

--- BIBLIOTEKA_PW/BIBLIOTEKA_PW.sql
+++ BIBLIOTEKA_PW/BIBLIOTEKA_PW.sql
@@ -1,16 +1,16 @@
 USE master;
-CREATE DATABASE LibraryPW2;
+CREATE DATABASE LibraryPW;
 GO
 
-USE LibraryPW2;
+USE LibraryPW;
 GO
 
-CREATE TYPE Bank_account_number FROM VARCHAR(26);
-CREATE TYPE Phone_number FROM VARCHAR(15); --15 bo w wielu krajach moze byc inna liczba znakow, jednak max=15
-CREATE TYPE Post_code FROM VARCHAR(8);
-CREATE TYPE Open_hours FROM VARCHAR(11); -- HH:MM-HH:MM
+CREATE TYPE bank_account_number FROM VARCHAR(30);
+CREATE TYPE phone_number FROM VARCHAR(20); --+48 111 111 111 
+CREATE TYPE post_code FROM VARCHAR(8);
+CREATE TYPE open_hours FROM VARCHAR(11); -- HH:MM-HH:MM
 
-USE LibraryPW2;
+USE LibraryPW;
 GO
 
 CREATE TABLE Authors(
@@ -21,7 +21,7 @@ biography NTEXT,
 username VARCHAR(50)
 );
 
-CREATE TABLE Status(
+CREATE TABLE Status(-- employees,deliveries,elementary_books, users
 status_id INT PRIMARY KEY IDENTITY(1,1),
 kind_of_status CHAR(50)
 );
@@ -36,25 +36,19 @@ category_id INT PRIMARY KEY IDENTITY (1,1),
 name VARCHAR(50)
 );
 
-CREATE TABLE Reviews(
-review_id INT PRIMARY KEY IDENTITY (1,1),
-mark INT NOT NULL CHECK (mark BETWEEN 1 AND 5),
-review VARCHAR(255),
-hide INT
-);
-
 CREATE TABLE Providers(
 provider_id INT PRIMARY KEY IDENTITY (1,1),
-company_name VARCHAR(50),
+company_name VARCHAR(60),
 adres VARCHAR(255),
-phone_number Phone_number
+phone_number phone_number
 );
 
 CREATE TABLE Invoices(
 invoice_id INT PRIMARY KEY IDENTITY (1,1),
 amount INT NOT NULL CHECK(amount > 0),
-payment VARCHAR(50) NOT NULL,
-bank_account_number Bank_account_number
+status_id int,
+bank_account_number bank_account_number
+FOREIGN KEY (status_id) REFERENCES Status(status_id)
 );
 
 CREATE TABLE Locations(
@@ -62,25 +56,14 @@ location_id INT PRIMARY KEY IDENTITY (1,1),
 city VARCHAR(50) NOT NULL,
 street VARCHAR(50) NOT NULL,
 number VARCHAR(10), -- 999/BBB/999
-post_code Post_code
+post_code post_code
 );
 
 CREATE TABLE Open_hours(
 open_hour_id INT PRIMARY KEY IDENTITY (1,1),
-day_of_week VARCHAR(20),
-hours Open_hours
+hours open_hours
 );
 
-CREATE TABLE Employees(
-employee_id INT PRIMARY KEY IDENTITY (1,1),
-phone_number Phone_number NOT NULL,
-salary INT NOT NULL CHECK(salary > 0),
-working_hours INT CHECK(working_hours > 0),
-status_id INT,
-login VARCHAR(20) UNIQUE,
-password VARCHAR(100),
-FOREIGN KEY (status_id) REFERENCES Status(status_id)
-);
 
 CREATE TABLE Extensions(
 extension_id INT PRIMARY KEY IDENTITY (1,1),
@@ -92,17 +75,24 @@ type VARCHAR(20)
 CREATE TABLE Books(
 book_id INT PRIMARY KEY IDENTITY (1,1),
 author_id INT,
-description VARCHAR(255),
-title VARCHAR(50) NOT NULL,
+description ntext,
+title VARCHAR(100) NOT NULL,
 publishing_year INT CHECK(publishing_year > 0),
 publishing_house_id INT,
 category_id INT,
-review_id INT,
 numbers_of_books INT CHECK(numbers_of_books >= 0),
 FOREIGN KEY (author_id) REFERENCES Authors(author_id),
 FOREIGN KEY (publishing_house_id) REFERENCES Publishing_houses(publishing_house_id),
 FOREIGN KEY (category_id) REFERENCES Categories(category_id),
-FOREIGN KEY (review_id) REFERENCES Reviews(review_id)
+);
+
+CREATE TABLE Reviews(
+review_id INT PRIMARY KEY IDENTITY (1,1),
+mark INT NOT NULL CHECK (mark BETWEEN 1 AND 5),
+review ntext,
+hide INT,
+book_id INT
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 CREATE TABLE Deliveries(
@@ -120,13 +110,26 @@ FOREIGN KEY (status_id) REFERENCES Status(status_id)
 
 CREATE TABLE Libraries(
 library_id INT PRIMARY KEY IDENTITY (1,1),
-name VARCHAR(50),
+name VARCHAR(100),
 location_id INT,
 open_hour_id INT,
-employee_id INT,
 FOREIGN KEY (location_id) REFERENCES Locations(location_id),
 FOREIGN KEY (open_hour_id) REFERENCES Open_hours(open_hour_id),
-FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+);
+
+CREATE TABLE Employees(
+employee_id INT PRIMARY KEY IDENTITY (1,1),
+name varchar(50),
+surename varchar(100),
+phone_number phone_number NOT NULL,
+salary INT NOT NULL CHECK(salary > 0),
+working_hours_per_day INT CHECK(working_hours_per_day > 0),
+status_id INT,
+login VARCHAR(20) UNIQUE,
+password VARCHAR(100),
+library_id int
+FOREIGN KEY (library_id) REFERENCES Libraries(library_id),
+FOREIGN KEY (status_id) REFERENCES Status(status_id)
 );
 
 CREATE TABLE Elementary_books(
@@ -144,13 +147,11 @@ CREATE TABLE Users(
 user_id INT PRIMARY KEY IDENTITY (1,1),
 name VARCHAR(50) NOT NULL,
 email VARCHAR(256) NOT NULL,
-phone_number Phone_number,
+phone_number phone_number,
 status_id INT,
-rent_id INT,
 indeks INT UNIQUE NOT NULL,
 FOREIGN KEY (status_id) REFERENCES Status(status_id)
 );
-
 CREATE TABLE Orders(
 order_id INT PRIMARY KEY IDENTITY (1,1),
 elementary_book_id INT,
@@ -160,6 +161,24 @@ pickup_time INT CHECK(pickup_time >= 0),
 FOREIGN KEY (user_id) REFERENCES Users(user_id),
 FOREIGN KEY (elementary_book_id) REFERENCES Elementary_books(elementary_book_id)
 );
+
+CREATE TABLE Rents(
+rent_id INT PRIMARY KEY IDENTITY (1,1),
+user_id INT,
+elementary_book_id INT,
+rent_date DATETIME NOT NULL,
+return_date DATE NOT NULL,
+rental_time INT,
+order_id INT,
+extension_id INT,
+employee_id INT,
+FOREIGN KEY (user_id) REFERENCES Users(user_id),
+FOREIGN KEY (elementary_book_id) REFERENCES Elementary_books(elementary_book_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (extension_id) REFERENCES Extensions(extension_id),
+FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+);
+
 
 CREATE TABLE Notifications(
 notification_id INT PRIMARY KEY IDENTITY (1,1),
@@ -172,31 +191,14 @@ FOREIGN KEY (extension_id) REFERENCES Extensions(extension_id),
 FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Rents(
-rent_id INT PRIMARY KEY IDENTITY (1,1),
-user_id INT,
-elementary_book_id INT,
-rent_date DATETIME NOT NULL,
-return_date DATE NOT NULL,
-rental_time INT CHECK(rental_time > 0),
-order_id INT,
-extension_id INT,
-employee_id INT,
-FOREIGN KEY (user_id) REFERENCES Users(user_id),
-FOREIGN KEY (elementary_book_id) REFERENCES Elementary_books(elementary_book_id),
-FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-FOREIGN KEY (extension_id) REFERENCES Extensions(extension_id),
-FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
-);
-
 CREATE TABLE Config(
 config_id INT PRIMARY KEY IDENTITY (1,1),
 name VARCHAR(50),
-value INT,
+value VARCHAR(50),
 modification_date DATETIME
 );
 
-USE LibraryPW2;
+USE LibraryPW;
 GO
 
 CREATE VIEW Book_view AS
@@ -210,13 +212,13 @@ publishing_houses.name AS publishing_house,
 categories.name AS category,
 reviews.mark,
 reviews.review
-FROM books
+FROM Books
 LEFT JOIN 
 publishing_houses ON books.publishing_house_id = publishing_houses.publishing_house_id
 LEFT JOIN
 categories ON books.category_id = categories.category_id
 LEFT JOIN
-reviews ON books.review_id = reviews.review_id;
+reviews ON books.book_id = reviews.book_id;
 
 GO
 
